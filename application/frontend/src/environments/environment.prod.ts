46f903f87c3b3c60a3aff08850c628fd4a70379b
Manjeet Thadani <manjeet.thadani@gmail.com>
2022-05-23 04:06:14 +0530

    Update backend prod url

--- application/frontend/src/environments/environment.prod.ts
+++ application/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // TODO: update this
-  apiURL: 'http://localhost:8000'
+  apiURL: 'http://a2e8d5688a0594658bfefb701419163f-239422344.us-west-2.elb.amazonaws.com/api/v1/hour-image'
 };
